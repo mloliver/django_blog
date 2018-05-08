47e03f753c0e332f71cc7ed7c02b70c8627f3420
Mike <mike@dev>
2018-05-07 21:20:00 -0500

    removes tags

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from taggit.managers import TaggableManager
+from django.db.models import Count
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
